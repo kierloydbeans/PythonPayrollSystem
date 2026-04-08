49f65503b0f4217cfd18264bf7cb4afdf683fca8
kierloydbeans <schofieldkier09@gmail.com>
2026-04-08 23:03:41 +0800

    added calculate salary function

--- PayrollSystem.py
+++ PayrollSystem.py
@@ -20,7 +20,7 @@ def starting():
         starting()
 
 def login():
-    db = open("users.txt")
+    # db = open("users.txt")
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     hashedPassword = hashPassword(password)
@@ -28,12 +28,16 @@ def login():
     try:
         with open("users.txt", "r") as f:
             for line in f:
-                storedUser, storedPass = line.strip().split(",")
-                if username == storedUser and hashedPassword == storedPass:
-                    print("Login success!")
-                    menu(username)
-                    return username
-        print("Invalid username or password.\n")
+                parts = line.strip().split(",")
+                if len(parts) >= 2:
+                    storedUser = parts[0]
+                    storedPass = parts[1]
+                    if username == storedUser and hashedPassword == storedPass:
+                        print("Login success!")
+                        menu(username)
+                        return
+                    else:
+                        print("Invalid username or password.\n")
         starting()
         return False
     except FileNotFoundError:
@@ -58,6 +62,8 @@ def menu(username):
         time_in(username)
     elif action == "2":
         time_out(username)
+    elif action == "4":
+        calculate_salary(username)
     elif action == "5":
         print(f"Logging out... Goodbye {username}!\n")
         starting()
@@ -79,4 +85,46 @@ def time_out(username):
     print("Time out recorded:", now)
     menu(username)
 
+def calculate_salary(username):
+    total_hours = 0
+    time_in_val = None
+    rate = 0.0
+
+# get rate
+    try:
+        with open(f"users.txt","r") as file:
+            for line in file:
+                parts = line.strip().split(",")
+                if parts[0] == username:
+                    rate = float(parts[2]) if len(parts) > 2 else 0.0
+                    break
+    except (FileNotFoundError, ValueError):
+        rate = 0.0
+
+# computation
+    try:
+        with open(f"logs_{username}.txt", "r") as file:
+            for line in file:
+                if ": " in line:
+                    tag, timestamp_str = line.strip()   .split(": ", 1)
+                    t_obj = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+
+                    if tag == "TIME IN":
+                        time_in_val = t_obj
+                    elif tag == "TIME OUT" and time_in_val:
+                        duration = t_obj - time_in_val
+                        total_hours += duration.total_seconds() / 3600
+                        time_in_val = None
+
+        salary = total_hours * rate
+        print("-"*30)
+        print(f"Total Hours: {total_hours:.2f}")
+        print(f"Hourly Rate: ₱{rate:.2f}")
+        print(f"GROSS PAY: ₱{salary:.2f}")
+        print("-" * 30)
+    except FileNotFoundError:
+        print("No logs found for the user.")
+
+    input("\n Press Enter to return to home")
+    menu(username)
 starting()
